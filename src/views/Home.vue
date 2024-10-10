682b20b9ab03c9271fc5e1176f3104892f0e56d8
Fábrica Software 2 <sysdesarrolladorfsw2@usfq.edu.ec>
2024-10-10 16:08:23 -0500

    Add cloudinary

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,37 @@
+<script setup>
+import { onMounted, ref } from "vue";
+const isDisabled = ref(false);
+const uploadedImages = ref([]);
+const cloudName = import.meta.env.VITE_CLOUD_NAME;
+const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+
+const uploadWidget = () => {
+  isDisabled.value = true;
+  console.log({ cloudName, uploadPreset });
+};
+</script>
+
 <template>
-  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-    <div class="col">
-      <div class="card mb-4 rounded-3 shadow-sm">
-        <div class="card-header py-3">
-          <h4 class="my-0 fw-normal">Free</h4>
-        </div>
-        <div class="card-body">
-          <h1 class="card-title pricing-card-title">
-            $0<small class="text-body-secondary fw-light">/mo</small>
-          </h1>
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>10 users included</li>
-            <li>2 GB of storage</li>
-            <li>Email support</li>
-            <li>Help center access</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-outline-primary">
-            Sign up for free
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card mb-4 rounded-3 shadow-sm">
-        <div class="card-header py-3">
-          <h4 class="my-0 fw-normal">Pro</h4>
-        </div>
-        <div class="card-body">
-          <h1 class="card-title pricing-card-title">
-            $15<small class="text-body-secondary fw-light">/mo</small>
-          </h1>
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>20 users included</li>
-            <li>10 GB of storage</li>
-            <li>Priority email support</li>
-            <li>Help center access</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-primary">
-            Get started
-          </button>
-        </div>
-      </div>
-    </div>
-    <div class="col">
+  <div class="row mb-3 text-center justify-content-center">
+    <div class="col-md-4">
       <div class="card mb-4 rounded-3 shadow-sm border-primary">
-        <div class="card-header py-3 text-bg-primary border-primary">
-          <h4 class="my-0 fw-normal">Enterprise</h4>
+        <div class="card-header py-3">
+          <h4 class="my-0 fw-normal">Cargar Fotos</h4>
         </div>
         <div class="card-body">
-          <h1 class="card-title pricing-card-title">
-            $29<small class="text-body-secondary fw-light">/mo</small>
-          </h1>
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>30 users included</li>
-            <li>15 GB of storage</li>
-            <li>Phone and email support</li>
-            <li>Help center access</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-primary">
-            Contact us
+          <button
+            type="button"
+            :class="{
+              'w-100': true,
+              'btn-primary': true,
+              btn: true,
+              'btn-sm': true,
+              'btn-disabled': isDisabled,
+            }"
+            :disabled="isDisabled"
+            @click="uploadWidget"
+          >
+            Subir
           </button>
         </div>
       </div>
